e1f75c23b6c21f60045c4a143f665b332d1177f6
Monseleon21 <99167815+monseleon21@users.noreply.github.com>
2024-09-24 17:46:14 -0600

    add IntroLaravel

---
+++ IntroLaravel/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    @vite(['resources/js/app.js'])
+    <title>Document</title>
+</head>
+<body>
+    Hola mundo Laravel Proyecto IntroLaravel
+</body>
+</html>
