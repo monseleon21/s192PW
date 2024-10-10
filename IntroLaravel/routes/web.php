029790e0cf7079ef643473380216e7aad21d37b1
Monseleon21 <99167815+monseleon21@users.noreply.github.com>
2024-10-09 19:01:44 -0600

    Practica controladores

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -2,22 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\controladorVistas;
+
+
+Route::get('/', [controladorVistas::class, 'home'])->name('rutainicio');
+
+Route::get('/formu', [controladorVistas::class, 'formulario'])->name('rutaform');
+
+Route::get('/cliente', [controladorVistas::class, 'consulta'])->name('rutaclientes');
+
+Route::view('/component','componentes')->name('rutacomponentes');
+
+
+
+
+
+
+
+
+
 //RUTA TIPO GET
 /* Route::get('/', function () {
     return view('welcome');
 }); */
 
 // RUTA TIPO VIEW
-Route::view('/','inicio')->name ('rutainicio');
+/*     Route::view('/','inicio')->name ('rutainicio');
 
 
-//CREACION DE RURTA DE NUESTRO FORMULARIO DE VISTA 
-//RUTA TIPO VIEW
-Route::view('/formu','formulario')->name ('rutaform');
+    //CREACION DE RURTA DE NUESTRO FORMULARIO DE VISTA 
+    //RUTA TIPO VIEW
+    Route::view('/formu','formulario')->name ('rutaform');
 
 
-// Ruta de clientes
-Route::view('/cliente','clientes')->name('rutaclientes');
+    // Ruta de clientes
+    Route::view('/cliente','clientes')->name('rutaclientes');
 
-// Ruta de componentes
-Route::view('/component','componentes')->name('rutacomponentes');
+    // Ruta de componentes
+     */
