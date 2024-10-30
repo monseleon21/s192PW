6d48c059be8d440f9fb16404a6d5fd2311c66606
Monseleon21 <99167815+monseleon21@users.noreply.github.com>
2024-10-30 02:11:03 -0600

    Creacion de controlador

--- PracticaRepasoll/app/Http/Controllers/controladorVistas.php
+++ PracticaRepasoll/app/Http/Controllers/controladorVistas.php
@@ -4,7 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class controladorVistas extends Controller
 {
-    //
+    
+    public function home (){
+        return view('principal');
+    } 
+
+    public function registro (){
+        return view('registro');
+    } 
+
+    public function procesarlibro(Request $peticion){
+        //return view('Se guardo el libro');
+       return $peticion->all();
+    }
 }
